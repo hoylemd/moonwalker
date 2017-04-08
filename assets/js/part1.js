efa6972a8bb41570d52562d9da75cd6638800906
Michael D. Hoyle <hoylemd@gmail.com>
2017-04-08 18:17:02 -0400

    load some assets

--- assets/js/part1.js
+++ assets/js/part1.js
@@ -1,14 +1,18 @@
-
-function preload() {
-}
-
-function create() {
-}
-
-function update() {
-}
-
 function start_game() {
+  function preload() {
+    this.load.image('sky', 'assets/img/sky.png');
+    this.load.image('ground', 'assets/img/platform.png');
+    this.load.image('star', 'assets/img/star.png');
+    this.load.spritesheet('dude', 'assets/img/dude.png', 32, 48);
+  }
+
+  function create() {
+  }
+
+  function update() {
+  }
+
+
   return new Phaser.Game(800,
                          600,
                          Phaser.AUTO,
