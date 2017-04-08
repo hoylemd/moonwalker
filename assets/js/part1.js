9cf1a0d431da6e1d6d0beaaf6998142185d69c7e
Michael D. Hoyle <hoylemd@gmail.com>
2017-04-08 18:24:43 -0400

    Add sky and platforms

--- assets/js/part1.js
+++ assets/js/part1.js
@@ -1,4 +1,6 @@
 function start_game() {
+  var platforms = null;
+
   function preload() {
     this.load.image('sky', 'assets/img/sky.png');
     this.load.image('ground', 'assets/img/platform.png');
@@ -7,6 +9,20 @@ function start_game() {
   }
 
   function create() {
+    this.physics.startSystem(Phaser.Physics.ARCADE);
+    this.add.sprite(0, 0, 'sky');
+    platforms = this.add.group();
+    platforms.enableBody = true;
+
+    var ground = platforms.create(0, this.world.height - 64, 'ground');
+    ground.scale.setTo(2, 2);
+    ground.body.immovable = true;
+
+    var ledge1 = platforms.create(400, 400, 'ground');
+    ledge1.body.immovable = true;
+
+    var ledge2 = platforms.create(-150, 250, 'ground');
+    ledge2.body.immovable = true;
   }
 
   function update() {
