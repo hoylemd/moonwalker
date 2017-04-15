c41e7f6ac8c6ad0027701a3f9aa0873f94dd5ffb
Michael D. Hoyle <hoylemd@gmail.com>
2017-04-15 13:56:04 -0400

    add watchify

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 window.PIXI = require( 'phaser/build/custom/pixi' );
 window.p2 = require( 'phaser/build/custom/p2' );
 window.Phaser = require( 'phaser/build/custom/phaser-split' );
+
 console.log('Hello, world');
 console.log(Phaser);
