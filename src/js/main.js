7ddf11dedd0e1bf72e42d02a119d00810233b262
Michael D. Hoyle <hoylemd@gmail.com>
2017-04-15 14:19:39 -0400

    move everything into dist (to be renamed static)

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,17 @@
 let Phaser = require('./vendor').phaser;
 
+let play_state = {
+  preload: function () {
+    this.game.load.image('background', 'images/background.png');
+  }
+};
+
 function main () {
   let game = new Phaser.Game(960, 600, Phaser.AUTO, 'mikes_game');
+
+  game.state.add('play', play_state);
+
+  game.state.start('play');
 }
 
 module.exports = main;
