d51aa46dbf99b0d854305eb3e5b9223413defb67
Michael D. Hoyle <hoylemd@gmail.com>
2017-04-15 13:36:46 -0400

    make browserify work

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
+window.PIXI = require( 'phaser/build/custom/pixi' );
+window.p2 = require( 'phaser/build/custom/p2' );
+window.Phaser = require( 'phaser/build/custom/phaser-split' );
 console.log('Hello, world');
+console.log(Phaser);
