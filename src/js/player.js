36f1ba9de8c6c1ef6a47123eb4c40af69208f6bb
Michael D. Hoyle <hoylemd@gmail.com>
2017-04-15 15:26:16 -0400

    adjust player anchor

--- src/js/player.js
+++ src/js/player.js
@@ -2,6 +2,8 @@ let Phaser = require('./vendor').phaser;
 
 function Player(game, x, y) {
   Phaser.Sprite.call(this, game, x, y, 'player');
+
+  this.anchor.set(0.5, 0.5);
 }
 
 Player.prototype = Object.create(Phaser.Sprite.prototype);
