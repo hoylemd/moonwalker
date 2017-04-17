72430b16cf020778fd7e67d7e30ce8a6845a2d50
Michael D. Hoyle <hoylemd@gmail.com>
2017-04-16 20:21:39 -0400

    fill out spider

--- src/js/spider.js
+++ src/js/spider.js
@@ -1,8 +1,20 @@
 let Phaser = require('./vendor').phaser;
 
 function Spider(game, x, y) {
+  Phaser.Sprite.call(this, game, x, y, 'spider');
 
+  this.anchor.set(0.5, 0.5);
+
+  this.animations.add('crawl', [0, 1, 2], 8, true);
+  this.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12);
+  this.animations.play('crawl');
+
+  this.game.physics.enable(this);
+  this.body.collideWorldBounds = true;
+  this.body.velocity.x = Spider.SPEED;
 }
+
+Spider.SPEED = 100;
 
 Spider.prototype = Object.create(Phaser.Sprite.prototype);
 Spider.prototype.constructior = Spider;
