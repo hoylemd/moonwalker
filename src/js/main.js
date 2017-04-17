76512c2e6649a302e16bee7b206c01630165775b
Michael D. Hoyle <hoylemd@gmail.com>
2017-04-16 20:10:29 -0400

    player can pickup coins

--- src/js/main.js
+++ src/js/main.js
@@ -33,8 +33,11 @@ let play_state = {
     coin.anchor.set(0.5, 0.5);
     coin.animations.add('rotate', [0, 1, 2, 1], 6, true); // 6fps, looped
     coin.animations.play('rotate');
+    this.game.physics.enable(coin);
+    coin.body.allowGravity = false;
     return coin;
   },
+
   handle_input: function() {
     let direction = 0;
     if (this.keys.left.isDown) {
@@ -47,7 +50,14 @@ let play_state = {
     this.player.move(direction);
   },
   handle_collisions: function() {
-    this.game.physics.arcade.collide(this.player, this.platforms);
+    let physics = this.game.physics.arcade;
+    physics.collide(this.player, this.platforms);
+
+    physics.overlap(this.player, this.coins, this.pickup_coin, null, this);
+  },
+
+  pickup_coin: function(player, coin) {
+    coin.kill();
   },
 
   init: function () {
