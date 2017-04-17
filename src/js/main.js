d17d37550f23defd62fdc8345af81d4a617811d5
Michael D. Hoyle <hoylemd@gmail.com>
2017-04-17 17:48:51 -0400

    rename file

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 let Phaser = require('./vendor').phaser;
-let LevelState = require('./play_state');
+let LevelState = require('./level_state');
 
 function main () {
   let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
