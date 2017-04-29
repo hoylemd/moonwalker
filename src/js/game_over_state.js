09a057e0121e1b0457e07e5c221163326bf4926e
Michael D. Hoyle <hoylemd@gmail.com>
2017-04-29 15:54:54 -0400

    Add the restart button

--- src/js/game_over_state.js
+++ src/js/game_over_state.js
@@ -15,6 +15,7 @@ function GameOverState(game) {
       console.log('clicked');
     };
 
+    this.ui = this.game.add.group();
     button = game.add.button(game.world.centerX - 95,                           // x
                              400,                                               // y
                              'grey_ui',                                         // spritesheet name
@@ -22,7 +23,26 @@ function GameOverState(game) {
                              this,                                              // callback context
                              'grey_button01.png',                               // over frame
                              'grey_button03.png',                               // down frame
-                             'grey_button02.png');                              // up frame
+                             'grey_button02.png',                               // up frame
+                             null,
+                             this.ui);
+    button.onInputDown.add(function () {
+      button.y += 4;
+    }, this);
+    button.onInputUp.add(function () {
+      button.y -= 4;
+    }, this);
+
+    button_text = this.game.add.text(button.width / 2,
+                                     button.height / 2,
+                                     'Restart',
+                                     {font: 'origicide',
+                                      fill: '#666',
+                                      stroke: '#999',
+                                      strokeThickness: 4,
+                                      fontSize: 32});
+    button_text.anchor.set(0.5, 0.5);
+    button.addChild(button_text);
   };
 
   this.update = function () {
