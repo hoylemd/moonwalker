6a321979729e97860d38604371bd6f7a18b338b9
Michael D. Hoyle <hoylemd@gmail.com>
2017-04-29 18:09:57 -0400

    reorganize game over for win/lose

--- src/js/game_over_state.js
+++ src/js/game_over_state.js
@@ -24,64 +24,18 @@ function GameOverState(game) {
     let message = new UILabel(this.game,
                               this.game.world.centerX,
                               150,
-                              self.win ? 'You Won!' : 'You Lost!');
+                              this.win ? 'You Won!' : 'You Lost!');
     message.anchor.set(0.5, 0.5);
     this.ui.add(message);
 
-    let none_button = new UIButton(this.game,
-                               this.game.world.centerX - (190 + 20 + 95),
-                               350,
-                               'Grey',
-                               actionOnClick,
-                               this);
-    this.ui.add(none_button);
-
-    red_button = new UIButton(this.game,
-                              this.game.world.centerX - (0 + 0 + 95),
+    let button = new UIButton(this.game,
+                              this.game.world.centerX - 95,
                               350,
-                              'Red',
+                              'Restart',
                               actionOnClick,
                               this,
-                              'red');
-    this.ui.add(red_button);
-
-    green_button = new UIButton(this.game,
-                                this.game.world.centerX + (95 + 20),
-                                350,
-                                'Green',
-                                actionOnClick,
-                                this,
-                                'green');
-    this.ui.add(green_button);
-
-    blue_button = new UIButton(this.game,
-                               this.game.world.centerX - (190 + 20 + 95),
-                               400,
-                               'Blue',
-                               actionOnClick,
-                               this,
-                               'blue');
-    this.ui.add(blue_button);
-
-    yellow_button = new UIButton(this.game,
-                                 this.game.world.centerX - (0 + 0 + 95),
-                                 400,
-                                 'Yellow',
-                                 actionOnClick,
-                                 this,
-                                 'yellow');
-    this.ui.add(yellow_button);
-
-    orange_button = new UIButton(this.game,
-                                 this.game.world.centerX + (95 + 20),
-                                 400,
-                                 'Orange',
-                                 actionOnClick,
-                                 this,
-                                 'orange');
-    this.ui.add(orange_button);
-
-
+                              this.win ? 'green' : 'red');
+    this.ui.add(button);
   };
 }
 
